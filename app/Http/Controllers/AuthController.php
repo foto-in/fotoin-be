18c53348344e6dddba5ba9451109c0440c59c8f8
Rin4th <rizalnur.art@gmail.com>
2024-03-19 02:54:20 +0700

    feat: implmement login functionality
    Implement login endpoint using Laravel validation.
    Validate user on login using username and password.
    Redirect to home page if sucessful otherwise go back with error message.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('username', $request->username)->first();
+        if (!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'username' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $isPhotographer = Photographer::where('user_id', $user->id)->exists();
+
+    }
 }

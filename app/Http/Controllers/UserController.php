c0f3f28e2218f5038c5700820fb4121f1c73a82d
rin4th <rizalnur@gmail.com>
2024-06-15 10:48:16 +0700

    feat: add photographer and portofolio model
    Add Photographer and Portofolio model to support user management.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Photographer;
+use App\Models\Portofolio;
 use Illuminate\Support\Facades\Storage;
 
 
